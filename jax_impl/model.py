c05133c67d4565e4f1f696b6e96c052d91ae06dc
Lucas <me@lucasshen.org>
2022-09-28 14:43:07 +0800

    special init is decoupled with module naming, but scattered

--- jax_impl/model.py
+++ jax_impl/model.py
@@ -14,7 +14,7 @@ import numpy as np
 from general.utils import ModelConfig
 
 
-class CausalMultiheadAttention(nn.Module):
+class Attention(nn.Module):
     config: ModelConfig
 
     @nn.compact
@@ -22,7 +22,10 @@ class CausalMultiheadAttention(nn.Module):
         b, t, c = x.shape
 
         # [q, k, v]
-        qkv = nn.Dense(features=3 * self.config.n_embd)(x).split(3, axis=-1)
+        qkv = nn.Dense(
+            features=3 * self.config.n_embd, 
+            kernel_init=nn.initializers.normal(stddev=0.02) # bias init default zeros
+            )(x).split(3, axis=-1)
         qkv = tree_map(
             lambda x: x.reshape(b, t, self.config.n_head, c // self.config.n_head).transpose((0, 2, 1, 3)),
             qkv,
@@ -42,6 +45,11 @@ class CausalMultiheadAttention(nn.Module):
         # (b,t,h,hc) -> (b,t,c)
         output = output.transpose((0, 2, 1, 3)).reshape(b, t, c)
 
+        output = nn.Dense(  # ref 3
+            self.config.n_embd, 
+            kernel_init=nn.initializers.normal(stddev=0.02 / math.sqrt(2 * self.config.n_layer))
+            )(output)
+
         return output
 
     @functools.cached_property
@@ -53,18 +61,18 @@ class CausalMultiheadAttention(nn.Module):
         return mask
 
 
-class EncoderBlock(nn.Module):
+class Block(nn.Module):
     config: ModelConfig
 
     @nn.compact
     def __call__(self, x, training=True):
         attn_out = nn.LayerNorm()(x)
-        attn_out = x + CausalMultiheadAttention(self.config)(attn_out)
+        attn_out = x + Attention(self.config)(attn_out)
 
         mlp = [
-            nn.Dense(4 * self.config.n_embd),
+            nn.Dense(4 * self.config.n_embd, kernel_init=nn.initializers.normal(stddev=0.02)),
             nn.gelu,
-            nn.Dense(self.config.n_embd),
+            nn.Dense(self.config.n_embd, kernel_init=nn.initializers.normal(stddev=0.02 / math.sqrt(2 * self.config.n_layer))),  # ref 3
             nn.Dropout(self.config.recid_pdrop, deterministic=not training),
         ]
 
@@ -84,12 +92,20 @@ class Embedding(nn.Module):
         """idx: (b, t), the data is seq of number in this toy problem"""
         b, t = idx.shape
 
-        position_embedding = nn.Embed(self.config.context_window, self.config.n_embd)
+        position_embedding = nn.Embed(
+            self.config.context_window, 
+            self.config.n_embd,
+            embedding_init=nn.initializers.normal(stddev=0.02)
+            )
 
         # (t,n_embd) > (1,t,n_embd)
         wpe = position_embedding(jnp.arange(t))[None,]
 
-        token_embedding = nn.Embed(self.config.vocab_size, self.config.n_embd)
+        token_embedding = nn.Embed(
+            self.config.vocab_size, 
+            self.config.n_embd,
+            embedding_init=nn.initializers.normal(stddev=0.02)
+            )
         wte = token_embedding(idx)  # (b, t, n_embd)
 
         output = nn.Dropout(self.config.embd_pdrop)(
@@ -108,10 +124,22 @@ class GPT(nn.Module):
         x = Embedding(self.config)(x, training)
 
         for _ in range(self.config.n_layer):
-            x = EncoderBlock(self.config)(x, training)
+            x = Block(self.config)(x, training)
 
         x = nn.LayerNorm()(x)
 
         logits = nn.Dense(self.config.vocab_size)(x)
 
         return logits
+
+
+# reference
+# param speical init
+# 1. Dense default init https://flax.readthedocs.io/en/latest/_modules/flax/linen/linear.html
+# 2. layernorm init default to ones and zeros https://flax.readthedocs.io/en/latest/_modules/flax/linen/normalization.html#LayerNorm
+# 3. Reinitialize selected weights subject to the OpenAI GPT-2 Paper Scheme:
+#     #   > A modified initialization which accounts for the accumulation on the residual path with model depth. Scale
+#     #   > the weights of residual layers at initialization by a factor of 1/√N where N is the # of residual layers.
+#     #   >   -- GPT-2 :: https://openai.com/blog/better-language-models/
+#     #
+#     # Reference (Megatron-LM): https://github.com/NVIDIA/Megatron-LM/blob/main/megatron/model/gpt_model.py
